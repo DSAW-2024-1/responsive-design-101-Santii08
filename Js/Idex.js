6732476f79074750b671cb7098e4c8c56add973d
Santii08 <dan.santiago.rc2005@gmail.com>
2024-02-10 23:22:18 -0500

    first version

---
+++ Js/Idex.js
@@ -0,0 +1,12 @@
+/*window.sr = ScrollReveal();
+  sr.reveal('.scroll-letter', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  sr.reveal('.scroll-letterW', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+*/
