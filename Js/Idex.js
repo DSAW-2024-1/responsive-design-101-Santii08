c43c941ff883d1f2dd6b1f29901fae3d3cb50eda
Santii08 <dan.santiago.rc2005@gmail.com>
2024-02-11 20:08:08 -0500

    final version

--- Js/Idex.js
+++ Js/Idex.js
@@ -1,12 +1,7 @@
-/*window.sr = ScrollReveal();
-  sr.reveal('.scroll-letter', {
-    duration: 3000,
-    origin: 'bottom',
-    distance: '-100px'
+window.sr = ScrollReveal(); //Importamos la bibilioteca descargada
+  sr.reveal('.scroll-letter', {// configtura las animaciones para los elementos especificos con la clase "scroll-letter"
+    duration: 3000,//Tiempo de la animacion
+    origin: 'bottom', // Lugar de origen de la animacion
+  
   });
-  sr.reveal('.scroll-letterW', {
-    duration: 3000,
-    origin: 'left',
-    distance: '-100px'
-  });
-*/
+
